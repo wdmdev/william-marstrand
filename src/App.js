f65d8ad791c813528f2b7e8e10fb9c8f448430e4
William Marstrand <william.marstrand@gmail.com>
2020-01-25 12:13:41 +0100

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
        <h1>Significa</h1>
        <img src="logo.png" alt="Logo" /> 

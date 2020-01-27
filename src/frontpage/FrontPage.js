e89c04ad178fb121f8e8c67417095e85134b6de2
William Marstrand <william.marstrand@gmail.com>
2020-01-27 22:17:43 +0100

    New layout

--- src/frontpage/FrontPage.js
+++ src/frontpage/FrontPage.js
@@ -3,19 +3,19 @@ import './FrontPage.css';
 
 function FrontPage() {
     return (
-        <div class="container body">
-            <div class="row">
-                <div class="col-sm align-self-center">
-                    <h1 class="my-text">About Me</h1>
-                    <img src="./about_me.jpg" alt="test img" class="firstimg" />
+        <div className="container body">
+            <div className="row">
+                <div className="col-sm align-self-center">
+                    <h1 className="my-text">About Me</h1>
+                    <img src="./about_me.jpg" alt="test img" className="firstimg" />
                 </div>
-                <div class="col-sm">
-                    <h1 class="my-text">My Projects</h1>
-                    <img src="./mountain_sunset.jpg" alt="test img" class="firstimg" />
+                <div className="col-sm">
+                    <h1 className="my-text">My Projects</h1>
+                    <img src="./mountain_sunset.jpg" alt="test img" className="firstimg" />
                 </div>
-                <div class="col-sm">
-                    <h1 class="my-text">Contact</h1>
-                    <img src="./contact.jpg" alt="test img" class="firstimg" />
+                <div className="col-sm">
+                    <h1 className="my-text">Contact</h1>
+                    <img src="./contact.jpg" alt="test img" className="firstimg" />
                 </div>
             </div>
         </div>
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -3,37 +3,37 @@ import './Navbar.css';
 
 function NavBar() {
     return (
-        <div class="pos-f-t">
-            <div class="collapse" id="navbarToggleExternalContent">
-                <div class="bg-dark p-4">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm">
+        <div className="pos-f-t">
+            <div className="collapse" id="navbarToggleExternalContent">
+                <div className="bg-dark p-4">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm">
                                 <h3>
-                                <a href="#">About Me</a>    
+                                    <a href=".">About Me</a>
                                 </h3>
                             </div>
-                            <div class="col-sm">
+                            <div className="col-sm">
                                 <h3>
-                                <a href="#">My Projects</a>    
+                                    <a href=".">My Projects</a>
                                 </h3>
                             </div>
-                            <div class="col-sm">
+                            <div className="col-sm">
                                 <h3>
-                                <a href="#">Contact</a>    
+                                    <a href=".">Contact</a>
                                 </h3>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <nav class="navbar navbar-dark bg-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-dark bg-dark">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="logo">
-                    <p class="logo-text">Significa</p>
-                    <img src="./logo.png" width="5%" alt="logo" class="logo-img" />
+                <div className="logo">
+                    <p className="logo-text">Significa</p>
+                    <img src="./logo.png" width="5%" alt="logo" className="logo-img" />
                 </div>
             </nav>
         </div>

50dfbe65ffde34a3c21ff2eaf103ae5862acab12
William Marstrand <william.marstrand@gmail.com>
2020-02-01 11:13:30 +0100

    Resized images

--- src/frontpage/FrontPage.js
+++ src/frontpage/FrontPage.js
@@ -7,15 +7,15 @@ function FrontPage() {
             <div className="row">
                 <div className="col-sm">
                     <h1 className="mytext">About Me</h1>
-                    <img src="./about_me.jpg" alt="test img" className="firstimg" />
+                    <img src="./about_me_1000.jpg" alt="test img" className="firstimg" />
                 </div>
                 <div className="col-sm">
                     <h1 className="mytext">My Projects</h1>
-                    <img src="./mountain_sunset.jpg" alt="test img" className="firstimg" />
+                    <img src="./mountain_sunset_1000.jpg" alt="test img" className="firstimg" />
                 </div>
                 <div className="col-sm">
                     <h1 className="mytext">Contact</h1>
-                    <img src="./contact.jpg" alt="test img" className="firstimg" />
+                    <img src="./contact_1000.jpg" alt="test img" className="firstimg" />
                 </div>
             </div>
         </div>

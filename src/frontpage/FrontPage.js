044746b68d9fbd5549838cbba0ff4a1b809bed17
William Marstrand <william.marstrand@gmail.com>
2020-02-01 10:42:04 +0100

    Center text on images

--- src/frontpage/FrontPage.js
+++ src/frontpage/FrontPage.js
@@ -5,16 +5,16 @@ function FrontPage() {
     return (
         <div className="container body">
             <div className="row">
-                <div className="col-sm align-self-center">
-                    <h1 className="my-text">About Me</h1>
+                <div className="col-sm">
+                    <h1 className="mytext">About Me</h1>
                     <img src="./about_me.jpg" alt="test img" className="firstimg" />
                 </div>
                 <div className="col-sm">
-                    <h1 className="my-text">My Projects</h1>
+                    <h1 className="mytext">My Projects</h1>
                     <img src="./mountain_sunset.jpg" alt="test img" className="firstimg" />
                 </div>
                 <div className="col-sm">
-                    <h1 className="my-text">Contact</h1>
+                    <h1 className="mytext">Contact</h1>
                     <img src="./contact.jpg" alt="test img" className="firstimg" />
                 </div>
             </div>

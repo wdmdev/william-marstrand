b88b50a40cdf4c6fc1223a689d5f75c5ce721fa7
William Marstrand <william.marstrand@gmail.com>
2020-02-03 22:15:37 +0100

    Added link for images on front page

--- src/frontpage/FrontPage.js
+++ src/frontpage/FrontPage.js
@@ -1,25 +1,35 @@
+
+import {
+        Link
+} from "react-router-dom";
 import React from 'react';
 import './FrontPage.css';
 
 function FrontPage() {
-    return (
-            <div className="container body">
-                <div className="row">
-                    <div className="col-sm">
-                            <h1 className="mytext">About Me</h1>
-                            <img src="./about_me_1000.jpg" alt="test img" className="firstimg" />
-                    </div>
-                    <div className="col-sm">
-                            <h1 className="mytext">My Projects</h1>
-                            <img src="./mountain_sunset_1000.jpg" alt="test img" className="firstimg" />
-                    </div>
-                    <div className="col-sm">
-                            <h1 className="mytext">Contact</h1>
-                            <img src="./contact_1000.jpg" alt="test img" className="firstimg" />
-                    </div>
+        return (
+                <div className="container body">
+                        <div className="row">
+                                <div className="col-sm">
+                                        <Link to="/about" replace>
+                                                <h1 className="mytext">About Me</h1>
+                                                <img src="./about_me_1000.jpg" alt="test img" className="firstimg" />
+                                        </Link>
+                                </div>
+                                <div className="col-sm">
+                                        <Link to="/projects" replace>
+                                                <h1 className="mytext">My Projects</h1>
+                                                <img src="./mountain_sunset_1000.jpg" alt="test img" className="firstimg" />
+                                        </Link>
+                                </div>
+                                <div className="col-sm">
+                                        <Link to="/contact" replace>
+                                                <h1 className="mytext">Contact</h1>
+                                                <img src="./contact_1000.jpg" alt="test img" className="firstimg" />
+                                        </Link>
+                                </div>
+                        </div>
                 </div>
-            </div>
-    );
+        );
 }
 
 export default FrontPage;

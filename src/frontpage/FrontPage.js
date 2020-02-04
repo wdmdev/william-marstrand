783a137a1e7f029d9e668dbbf748d75038d0fb44
William Marstrand <william.marstrand@gmail.com>
2020-02-05 00:02:12 +0100

    Initial projects page

--- src/frontpage/FrontPage.js
+++ src/frontpage/FrontPage.js
@@ -1,35 +1,32 @@
-
-import {
-        Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from 'react';
 import './FrontPage.css';
 
 function FrontPage() {
-        return (
-                <div className="container body">
-                        <div className="row">
-                                <div className="col-sm">
-                                        <Link to="/about" replace>
-                                                <h1 className="mytext">About Me</h1>
-                                                <img src="./about_me_1000.jpg" alt="test img" className="firstimg" />
-                                        </Link>
-                                </div>
-                                <div className="col-sm">
-                                        <Link to="/projects" replace>
-                                                <h1 className="mytext">My Projects</h1>
-                                                <img src="./mountain_sunset_1000.jpg" alt="test img" className="firstimg" />
-                                        </Link>
-                                </div>
-                                <div className="col-sm">
-                                        <Link to="/contact" replace>
-                                                <h1 className="mytext">Contact</h1>
-                                                <img src="./contact_1000.jpg" alt="test img" className="firstimg" />
-                                        </Link>
-                                </div>
-                        </div>
-                </div>
-        );
+    return (
+		<div className="container body">
+			<div className="row">
+				<div className="col-sm">
+					<Link to="/about" replace>
+						<h1 className="myfronttext">About Me</h1>
+						<img src="./about_me_1000.jpg" alt="test img" className="firstimg" />
+					</Link>
+				</div>
+				<div className="col-sm">
+					<Link to="/projects" replace>
+						<h1 className="myfronttext">My Projects</h1>
+						<img src="./mountain_sunset_1000.jpg" alt="test img" className="firstimg" />
+					</Link>
+				</div>
+				<div className="col-sm">
+					<Link to="/contact" replace>
+						<h1 className="myfronttext">Contact</h1>
+						<img src="./contact_1000.jpg" alt="test img" className="firstimg" />
+					</Link>
+				</div>
+			</div>
+		</div>
+    );
 }
 
 export default FrontPage;
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import ProjectThumb from '../projectThumb/ProjectThumb';
 
 function Projects() {
     return(
-        <div class="container">
-            <p class="mytext">Projects in progress...</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm">
+                    <ProjectThumb href="https://github.com/wdmdev/Copenhacks2019"
+                                    src="./copenhacks_github.png"
+                                    text="Winner of Copenhack 2019"/>
+                </div>
+                <div className="col-sm">
+                    <ProjectThumb href="https://github.com/wdmdev/ASL-image-recognition"
+                                    src="ASL_github.jpg"
+                                    text="Recognizing ASL-Letters"
+                                    />
+                </div>
+                <div className="col-sm">
+                    <ProjectThumb href="https://github.com/wdmdev/valhacks2019"
+                                    src="smart_cantine_github.png"
+                                    text="Valhacks 2019 Contester"
+                                    />
+                </div>
+            </div>
         </div>
     );
 }

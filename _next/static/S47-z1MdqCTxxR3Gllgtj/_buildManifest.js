f01b4a5904a75c5d4874d6c46dd53f82c047d68d
wdmdev <wdmdevelop@gmail.com>
2022-11-15 11:42:38 +0000

    Deploying to live from @ wdmdev/william-marstrand@eb532d0b93df4695ee683dd6ca8ec24fe70ee183 🚀

--- _next/static/S47-z1MdqCTxxR3Gllgtj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-a46ae10782895f63.js","static/chunks/pages/index-13954f5a65a4aa14.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

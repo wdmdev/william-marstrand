eb532d0b93df4695ee683dd6ca8ec24fe70ee183
William Marstrand <william.marstrand@theorg.com>
2022-11-15 12:41:43 +0100

    Updated types

--- components/IntroCode.tsx
+++ components/IntroCode.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-const Code = ({ language, children }) => (
+type Props = {
+    language: string,
+    children: string
+}
+
+const Code = ({ language, children } : Props) => (
     <SyntaxHighlighter language={ language } style={ dracula } showLineNumbers={true} wrapLines={true}>
         { children }
     </SyntaxHighlighter>

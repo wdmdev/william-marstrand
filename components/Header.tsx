8b5babdc2dc7eaaa91b15455576815c7c5122b4c
William Marstrand <wdmdevelop@gmail.com>
2023-11-14 12:42:18 +0100

    better layout for mobile and some text updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,14 +9,10 @@ export default function Header() {
             <NavBar />
             <div className="header flex light-bg">
                 <div>
-                    <button>ML & Data Engineer</button>
-                    <h1 className="white">Under construction...</h1>
-                    {/* <h1 className="white">Anyone can collect data.</h1>
-                    <h1 className="white">But how do you utilize it?</h1>
-                    <p className="gray">I produce real life value from data.</p> */}
+                    <button>Data Scientist</button>
+                    <h1 className="cover-text white">Model {'>'} Deploy {'>'} Monitor {'>'} Repeat</h1>
                     <a className="green" href="mailto:wdmdevelop@gmail.com">Let&#39;s chat!</a>
                 </div>
-                <Image src="/images/..." width={463} height={513} alt="william"  /> 
             </div>
         </header>
     )

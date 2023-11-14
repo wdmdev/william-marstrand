6269b24090cc9bbe6847db864ab81e1c12a69a94
William Marstrand <wdmdevelop@gmail.com>
2023-11-14 20:54:13 +0100

    New cover image

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,7 @@ export default function Header() {
         <header>
             <NavBar />
             <div className="header flex light-bg">
-                <div>
-                    <button>Data Scientist</button>
-                    <h1 className="cover-text white">Model {'>'} Deploy {'>'} Monitor {'>'} Repeat</h1>
-                    <a className="green" href="mailto:wdmdevelop@gmail.com">Let&#39;s chat!</a>
-                </div>
+                <div className="inner-cover"> </div>
             </div>
         </header>
     )
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,13 @@ export default function NavBar() {
     return (
         <nav className="flex light-bg justify-space">
             <div className="logo white">
+                <div>
+                    <button>Data Scientist</button>
+                </div>
                 <Link href="/william-marstrand">William Marstrand</Link>
+                <div >
+                    <a className="green" href="mailto:wdmdevelop@gmail.com">Let&#39;s chat!</a>
+                </div>
             </div>
 
             <ul className="flex white">

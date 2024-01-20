5d5b3c12b6eba6426ca98b7611eb7edfda265a64
William Marstrand <wdmdevelop@gmail.com>
2024-01-20 21:24:11 +0100

    trying this path as a fix

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
  * @type {import('next').NextConfig} 
  */
  const nextConfig   = {
-  basePath: '/william-marstrand',
   images: {
-    loader: 'akamai',
-    path: '/william-marstrand',
+    loader: 'default',
+    path: '',
     unoptimized: true,
   },
+  assetPrefix: './',
   output: 'export'
 };
 

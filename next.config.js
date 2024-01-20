ed0b73d2cc206d11f19bae6bdd6c0661c39ce797
William Marstrand <wdmdevelop@gmail.com>
2024-01-20 21:27:02 +0100

    trying this based on github example

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
  * @type {import('next').NextConfig} 
  */
  const nextConfig   = {
+  output: 'export',
+  basePath: '/william-marstrand',
   images: {
-    loader: 'default',
-    path: '',
     unoptimized: true,
   },
-  assetPrefix: './',
-  output: 'export'
 };
 
 module.exports = nextConfig;
